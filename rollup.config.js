ff57f75f3ffdd69949bdc404dcb66058af69cd0b
berkinproxify <165379079+berkinproxify@users.noreply.github.com>
2024-05-15 10:51:27 +0100

    Update rollup.config.js
    rm unnecessary path

--- rollup.config.js
+++ rollup.config.js
@@ -35,8 +35,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js',
-		assetFileNames: 'https://github.com/petarproxify/Hackathon_April_2024/tree/gh-pages/assets/logos'
+		file: 'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
