9c201b6b128ebd95a6a2b7c80ea73a9c0116ba2d
berkinproxify <165379079+berkinproxify@users.noreply.github.com>
2024-05-15 11:01:03 +0100

    Update rollup.config.js
    update file path

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'https://github.com/petarproxify/Hackathon_April_2024/tree/gh-pages/public/build/bundle.js',
+		file: 'public/build/bundle.js',
 		assetFileNames: 'https://github.com/petarproxify/Hackathon_April_2024/tree/gh-pages/assets/logos'
 	},
 	plugins: [
